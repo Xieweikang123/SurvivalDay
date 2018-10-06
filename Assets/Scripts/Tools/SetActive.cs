c7a94bdda92f7c35cbbbb47fe394426ced7ed34d
xwk <570312124@qq.com>
2018-10-06 11:17:13 +0800

    新增小工具：SetActive脚本：alt+2  激活和失活对象

---
+++ Assets/Scripts/Tools/SetActive.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SetActive  {
+
+    [MenuItem("GameObject/显示所有子物体和父物体 &2", priority = -1)]
+    public static void SetActiveOrInactive()
+    {
+        GameObject selectionObj = Selection.activeGameObject;
+
+        selectionObj.SetActive(!selectionObj.activeSelf);
+    }
+}
