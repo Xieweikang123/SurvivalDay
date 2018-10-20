e6c5553717b077fbf764ed383d36ad8e70ed4b08
xwk <2303061691@qq.com>
2018-10-20 11:01:50 +0800

    用脚本控制播放动画（9/10）

--- Assets/Scripts/Controller/AnimController.cs
+++ Assets/Scripts/Controller/AnimController.cs
@@ -14,8 +14,8 @@ public class AnimController : MonoBehaviour
     [SerializeField]
     private Sprite[] spritesMoveRight;
 
-
-    private int index = 0;
+    private Sprite[] spritesMoveDir;    //移动动画数组，储存当前移动方向动画
+    private int currentAnimIndex = 0;  //当前动画下标
     private SpriteRenderer spriteRenderer;
     [SerializeField]
     private float playPeriod = 1.0f; //两帧之间播放间隔
@@ -25,78 +25,95 @@ public class AnimController : MonoBehaviour
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
+    //private void OnGUI()
+    //{
+    //    if (Input.anyKeyDown)
+    //    {
+    //        Event e = Event.current;
+    //        if (e.isKey)
+    //        {
+    //            print("keyCode down:  " + e.keyCode);
+    //        }
+    //    }
+    //}
 
     private void Update()
     {
         timer += Time.deltaTime;
 
+
         //首次按下，先播放下一帧动画，瞬间响应玩家反馈
         if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
-            spriteRenderer.sprite = spritesMoveDown[++index];
+            spritesMoveDir = spritesMoveDown;
+            timer = playPeriod;
         }
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
-            spriteRenderer.sprite = spritesMoveUp[++index];
+            spritesMoveDir = spritesMoveUp;
+            timer = playPeriod;
         }
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            spriteRenderer.sprite = spritesMoveLeft[++index];
+            spritesMoveDir = spritesMoveLeft;
+            timer = playPeriod;
         }
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
-            spriteRenderer.sprite = spritesMoveRight[++index];
+            spritesMoveDir = spritesMoveRight;
+            timer = playPeriod;
         }
         //如果键盘移动键抬起，则将动画置为0帧，使得玩家恢复至idle状态
-        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
+        if (Input.GetKeyUp(KeyCode.W)||Input.GetKeyUp(KeyCode.UpArrow)|| 
+            Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow)||
+            Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow)||
+            Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
         {
-            index = 0;
-            spriteRenderer.sprite = spritesMoveDown[index];
+            KeyLift();
         }
-        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow))
-        {
-            index = 0;
-            spriteRenderer.sprite = spritesMoveUp[index];
-        }
-        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
-        {
-            index = 0;
-            spriteRenderer.sprite = spritesMoveLeft[index];
-        }
-        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
-        {
-            index = 0;
-            spriteRenderer.sprite = spritesMoveRight[index];
-        }
-        if (timer > playPeriod)
+
+        if (timer >= playPeriod)
         {
             timer = 0;
 
-            //TODO: 不一定是这个动画
-            if (++index >= spritesMoveDown.Length-1)
-                index = 0;
-
-            print(index);
-
             if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
             {
-                spriteRenderer.sprite = spritesMoveDown[++index];
+                ChangeMoveSprite(spritesMoveDown);
+                return;
             }
             if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
             {
-                spriteRenderer.sprite = spritesMoveUp[++index];
+                ChangeMoveSprite(spritesMoveUp);
+                return;
             }
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
             {
-                spriteRenderer.sprite = spritesMoveLeft[++index];
+                ChangeMoveSprite(spritesMoveLeft);
+                return;
             }
             if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             {
-                spriteRenderer.sprite = spritesMoveRight[++index];
+                ChangeMoveSprite(spritesMoveRight);
+                return;
             }
-        
-        }
 
-       
+        }
+    }
+    //移动方向键抬起
+    private void KeyLift()
+    {
+        currentAnimIndex = 0;
+        spriteRenderer.sprite = spritesMoveDir[currentAnimIndex];
+    }
+
+    //改变移动动画帧
+    private void ChangeMoveSprite(Sprite[] spritesMoveDir)
+    {
+        
+        //如果当前动画下标+1后，超过了动画数组的长度，则重置为0
+        if (++currentAnimIndex == spritesMoveDir.Length)
+            currentAnimIndex = 0;
+
+        spriteRenderer.sprite = spritesMoveDir[currentAnimIndex];
     }
 }
